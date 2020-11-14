584a8641abe3fb12b6a770b1bdf0b622d642dbf4
lilxtcher <66361075+lilxtcher@users.noreply.github.com>
2020-11-14 16:01:33 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+#this is for installing modules and requirements#
+
+import os
+
+os.system("sudo apt-get install python")
+os.system("sudo apt-get install python3-pip")
+os.system("sudo pip3 install translate")
+os.system("sudo pip3 install SpeechRecognition")
+os.system("sudo pip3 install PyAudio")
